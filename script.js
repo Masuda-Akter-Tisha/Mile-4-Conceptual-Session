cc3b6791fa02d29658d921fb114406d58de75a7f
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-03-26 23:18:50 +0600

    created basic html file and added js file

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log (document.body);
+const heading = document.getElementById ('heading');
+console.log (heading);
+
+// const heading1 = document.querySelector ('#heading');
+// console.log (heading1);
+
+const body = document.querySelector ('.body');
+console.log (body);
