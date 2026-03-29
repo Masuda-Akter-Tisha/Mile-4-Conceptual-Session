4656b3ff34918d0e8594a5ea18b07d944aba45f3
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-03-29 23:16:09 +0600

    practiced Dom

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 console.log (document.body);
 const heading = document.getElementById ('heading');
-console.log (heading);
 
 // const heading1 = document.querySelector ('#heading');
 // console.log (heading1);
 
-const body = document.querySelector ('.body');
-console.log (body);
+const paragraph = document.querySelectorAll ('#paragraph');
+ console.log(paragraph);
+
+for (let p of paragraph) {
+    p.innerText = 'this is dom objects';
+}
+
+const text = document.querySelectorAll ('.text');
+text[0].innerText = 'dom-object';
+
+const clickBtn = document.getElementById ('handle');
+clickBtn.addEventListener ('click', function () {
+    text[0].classList.remove ('hidden');
+})
+
+const parent = document.getElementById ('text-container');
+
+const child = document.createElement ('p');
+child.innerText = 'This is a very important lessons of java script';
+
+parent.appendChild (child);
+
+const child2 = document.createElement ('div');
+child2.innerHTML = `
+<p> Dom -> document object model.</p>
+`;
+
+parent.appendChild (child2);
+
+const child3= document.createElement ('h1');
+child3.innerText = 'browser converted html file as a tree like object.';
+
+parent.appendChild (child3);
