a62789de0541a785eb6c4c11db43ddc350ce8c02
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-03 12:04:08 +0600

    3 btns toggling has been implemented

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// total,thriving,struggling counts function
 let total = document.getElementById ('total');
 let thrivingCount = document.getElementById ('thrivingCount');
 let strugglingCount = document.getElementById ('strugglingCount');
@@ -14,3 +15,22 @@ function calculateCount () {
 }
 
 calculateCount ();
+
+// 3 buttons toggling
+const allFilterBtn = document.getElementById ('all-filter-btn');
+const thrivingFilterBtn = document.getElementById ('thriving-filter-btn');
+const strugglingFilterBtn = document.getElementById ('struggling-filter-btn');
+
+function togglingStyle (id) {
+   allFilterBtn.classList.remove ('bg-black', 'text-white');
+   thrivingFilterBtn.classList.remove ('bg-black', 'text-white');
+   strugglingFilterBtn.classList.remove ('bg-black','text-white'); 
+   
+   allFilterBtn.classList.add ('bg-gray-300', 'text-black');
+   thrivingFilterBtn.classList.add ('bg-gray-300', 'text-black');
+   strugglingFilterBtn.classList.add ('bg-gray-300', 'text-black');
+
+   const selected = document.getElementById (id);
+   selected.classList.remove ('bg-gray-300', 'text-black');
+   selected.classList.add ('bg-black', 'text-white');
+}
